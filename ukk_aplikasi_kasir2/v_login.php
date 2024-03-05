1ea7ed4425e37c3990f740469b229f735824af62
Frhn1 <160454725+Frhn1@users.noreply.github.com>
2024-03-04 18:13:42 -0800

    Add files via upload

---
+++ ukk_aplikasi_kasir2/v_login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Login - Aplikasi Kasir</title>
+     <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+     <h1>APLIKASI KASIR</h1>
+     <form action="m_login.php" method="post">
+          <table>
+               <tr>
+                    <td>username</td>
+                    <td>:</td>
+                    <td><input type="text" name="username_login" id="" class="form-control"></td>
+               </tr>
+               <tr>
+                    <td>password</td>
+                    <td>:</td>
+                    <td><input type="password" name="password_login" id="" class="form-control"></td>
+               </tr>
+               <tr>
+                    <td></td>
+                    <td></td>
+                    <td><input type="submit" value="login"></td>
+                    <script src="css/bootstrap.min.css"></script>
+               </tr>
+          </table>
+     </form>
+</body>
+</html>
